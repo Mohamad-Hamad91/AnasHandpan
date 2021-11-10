a2761a18824a6a47cdf2b46f6185f182435a5a56
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-11-10 15:48:56 +0200

    all products

--- src/app/website/all-products/all-products.component.ts
+++ src/app/website/all-products/all-products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Product, ProductReq } from '../model/product';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-all-products',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProductsComponent implements OnInit {
 
-  constructor() { }
+  data: Product[] = new Array();
+  params: ProductReq = { PageNumber: 1, PageSize: 9 };
+  waiting: boolean = true;
+  baseURL: string = environment.baseURL;
+  totlaCount: number = 0;
+
+  constructor(private _dataService: ProductService) { }
 
   ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.waiting = true;
+    this._dataService.getAll(this.params)
+      .subscribe(res => {
+        this.data.push(...res.Data.List);
+        this.totlaCount = +res.Data.TotalCount;
+        this.waiting = false;
+      }, er => this.waiting = false);
+  }
+
+  loadMore() {
+    this.params.PageNumber++;
+    this.getData();
   }
 
 }
