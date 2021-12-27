cb5060c4d7e5e1481019777d2fdc655163a09cbd
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2021-12-27 14:56:29 +0200

    video bg and albums disk/columns

--- src/app/website/albums/albums.component.ts
+++ src/app/website/albums/albums.component.ts
@@ -11,10 +11,24 @@ export class AlbumsComponent implements OnInit {
 
   @Input('data') data: Album[] = new Array();
   baseURL = environment.baseURL;
+  floor = Math.floor;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    const disks: HTMLCollectionOf<Element> = document.getElementsByClassName('disk');
+    // debugger;
+    // for (let index = 0; index < disks.length; index++) {
+    //   const element:HTMLSpanElement = disks.item(index) as HTMLSpanElement;
+    //   element.style.backgroundImage = this.baseURL + this.data[index].Thumbnail;
+    // }
+  }
+
+  diskHover(e: MouseEvent, index: number) {
+    debugger;
+    const element:HTMLSpanElement = e.target as HTMLSpanElement;
+    element.style.backgroundImage = this.baseURL + this.data[index].Thumbnail;
   }
 
 }
--- src/app/website/cover/cover.component.ts
+++ src/app/website/cover/cover.component.ts
@@ -11,10 +11,14 @@ export class CoverComponent implements OnInit {
 
   @Input('data') data: Cover = new Cover();
   baseURL = environment.baseURL;
+  videoURL = 'bmzXk5E4gAs';
+  dataProperty="{videoURL:'bmzXk5E4gAs',containment:'.player',autoPlay:true, mute:true, startAt:0, opacity:1}";
 
   constructor() { }
 
   ngOnInit(): void {
+    const videoEl:HTMLElement = document.getElementById('bgndVideo');
+    videoEl.setAttribute('data-property', this.dataProperty);
   }
 
 }
