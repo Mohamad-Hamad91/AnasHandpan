22a487cdf7ee1e12f9e94ce48ae1a60b373e167d
IbrahimAdDandan <ibrahim.ad.dandan@gmail.com>
2022-02-14 13:39:46 +0200

    news fixing

--- src/app/website/news/news.component.ts
+++ src/app/website/news/news.component.ts
@@ -12,10 +12,12 @@ export class NewsComponent implements OnInit {
   @Input('data') data: News[] = new Array();
   baseURL: string = environment.baseURL;
   floor = Math.floor;
+  offset: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.offset = (this.data.length > 2 || window.innerWidth < 1000) ? 0 : this.data.length == 2 ? 11.5 : 33;
   }
 
 }
